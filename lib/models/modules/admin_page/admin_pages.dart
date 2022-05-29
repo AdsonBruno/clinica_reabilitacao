9ba788a19a121317b8db57ff74007cd953af5060
AdsonBruno <adsonbruno2@gmail.com>
2022-05-29 12:04:38 -0300

    refatoracao admin_pages e criando widget main_bar

--- lib/models/modules/admin_page/admin_pages.dart
+++ lib/models/modules/admin_page/admin_pages.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:clinica_reabilitacao/models/themes/app_colors.dart';
-import 'package:clinica_reabilitacao/models/themes/app_text_styles.dart';
-
 import 'package:clinica_reabilitacao/models/widget/card_widget.dart';
+import 'package:clinica_reabilitacao/models/widget/main_bar.dart';
 
 class AdminPage extends StatelessWidget {
   const AdminPage({Key? key}) : super(key: key);
@@ -21,44 +19,8 @@ class AdminPage extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: SizedBox(
-                      width: 61,
-                      height: 61,
-                      child: Row(children: [
-                        const Icon(
-                          Icons.exit_to_app,
-                          size: 30,
-                          color: AppColors.second,
-                        ),
-                        Text(
-                          'Sair',
-                          style: TextStyles.subtitleButton,
-                        ),
-                      ]),
-                    ),
-                  ),
-                  Container(
-                    width: 50,
-                    height: 50,
-                    // color: AppColors.black,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(18),
-                      // color: AppColors.black,
-                    ),
-                    child: Image.asset(
-                      'assets/icons/userIcon.png',
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 30),
+              const MainBarWidget(),
+              const SizedBox(height: 10),
               Flexible(
                 child: GridView.count(
                   primary: true,
