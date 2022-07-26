fec8ac6414c60be15db889365be4e06a7f133dc4
AdsonBruno <adsonbruno2@gmail.com>
2022-07-25 21:55:41 -0300

    refatorando main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:clinica_reabilitacao/models/modules/clinic_registration_page/clinic_registration_page.dart';
+import 'package:clinica_reabilitacao/models/modules/procedure_registration_page/procedure_registration_page.dart';
 import 'package:flutter/material.dart';
 import 'package:clinica_reabilitacao/models/modules/doctor_registration/doctor_registration_page.dart';
 import 'package:clinica_reabilitacao/models/modules/login/login_page.dart';
@@ -13,18 +15,15 @@ void main() {
       scaffoldBackgroundColor: AppColors.backgroundColor,
     ),
     debugShowCheckedModeBanner: false,
-
-    // home: const AdminPage(),
-    // home: const AppWidget(),
-    // home: const LoginPage(),
-    // home: const DoctorRegistrationPage(),
-    // Ao definir uma initialRoute, não definir uma Home
-    initialRoute: '/patientRegistrationPage',
+    initialRoute: '/loginPage',
     routes: {
       '/loginPage': (context) => const LoginPage(),
       '/adminPage': (context) => const AdminPage(),
       '/doctorRegistration': (context) => const DoctorRegistrationPage(),
       '/patientRegistrationPage': (context) => const PatientRegistrationPage(),
+      '/procedureRegistrationPage': (context) =>
+          const ProcedureRegistrationPage(),
+      '/clinicRegistrationPage': (context) => const ClinicRegistrationPage(),
     },
   ));
 }
